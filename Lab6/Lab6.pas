51d28be326c5cab7a8c18e6e793a8dce552a367a
MikeChuv <mchuvorkin@gmail.com>
2020-03-16 16:17:15 +0300

    Done

--- Lab6/Lab6.pas
+++ Lab6/Lab6.pas
@@ -1,24 +1,47 @@
-﻿program lab6;
+﻿//Если в одномерном массиве a из n элементов нет элементов, 
+//модуль которого попадает в заданный диапазон, 
+//найти сумму элементов массива, для которых выполняется условие a[i] ^ 2 > i. 
+
+program lab6;
 
 uses myRecursion;
+
+const 	INDEX_EXCEPTION = 'Недостаточно параметров';
+		FILE_EXCEPTION = 'Невозможно открыть файл';
+		INTERVAL_OUT_OF_RANGE = 'Границы диапазона заданы неверно';
 
 var fin,fout: textfile;
 	a: vec;
 	n: integer;
-
+	intervalBegin, intervalEnd: real;
+	thisSum: real;
 begin
 	try
 		assignfile(fin,ParamStr(1));
 		reset(fin);
 		assignfile(fout,ParamStr(2));
 		rewrite(fout);
-		readVec(a,fin);
-		writeVec(a,fout);
+		readln(fin, intervalBegin, intervalEnd);
+		if (intervalBegin < 0) or (intervalEnd < 0) or (intervalBegin > intervalEnd) then 
+			writeln(INTERVAL_OUT_OF_RANGE)
+		else begin
+			readVec(a,fin);
+			writeVec(a,fout);
+			if not areInInterval(a, 0, intervalBegin,intervalEnd) then begin
+				thisSum := sum(a);
+				writeln(fout,'Sum = ', thisSum);
+			end
+			else writeln(fout, 'Array has elements with abs in range: ', 
+						intervalBegin, ' ',intervalEnd);
+		end;
 		
 
+		
+		closefile(fin);
+		closefile(fout);
 	except
-	on System.IndexOutOfRangeException do writeln('Недостаточно параметров');
-	on System.IO.FileNotFoundException do writeln('Невозможно открыть файл');
+	on System.IndexOutOfRangeException do writeln(INDEX_EXCEPTION);
+	on System.IO.FileNotFoundException do writeln(FILE_EXCEPTION);
 	end;
     
 end.
--- Lab6/myRecursion.pas
+++ Lab6/myRecursion.pas
@@ -4,8 +4,25 @@ interface
 type vec = array of real;
 procedure readVec(var x: vec; f:textfile);
 procedure writeVec(const x: vec; f:textfile);
+function areInInterval(const x: vec; startIndex: integer; intervalBegin,intervalEnd:real):boolean;
+function sum(const x: vec):real;
+
 
 implementation
+
+function areInInterval(const x: vec; startIndex: integer; intervalBegin,intervalEnd:real):boolean;
+var stopIndex: integer;
+begin
+	stopIndex := High(x);
+	if startIndex = stopIndex then
+		result := (abs(x[startIndex]) > intervalBegin) and (abs(x[startIndex]) < intervalEnd)
+	else
+	begin
+		result := 	(abs(x[startIndex]) > intervalBegin) and 
+					(abs(x[startIndex]) < intervalEnd) or 
+					areInInterval(x, startIndex + 1, intervalBegin,intervalEnd);
+	end;
+end;
 
 procedure readVec(var x: vec; f:textfile);
 var n:integer;
@@ -22,12 +39,19 @@ procedure writeVec(const x: vec; f:textfile);
 var i: integer;
 begin
  	for i := 0 to High(x) do begin
-		write(x[i], '  ');
-		writeln(f, 'suck my dick');
 		write(f, x[i], '  ');
 	end;
 	writeln(f);
 end;
+
+function sum(const x: vec):real;
+begin
+	result := 0;
+	for i:integer :=0 to High(x) do begin
+		if power(x[i],2) > i then result += x[i];
+	end;
+end;
+
 
 
 end.
